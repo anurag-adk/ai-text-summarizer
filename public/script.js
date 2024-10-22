4a286085ef0ceaa7ca227e866bf84db4647f1c71
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-22 20:26:34 +0545

    client side done

--- public/script.js
+++ public/script.js
@@ -1,1 +1,56 @@
+const textArea = document.getElementById("text_to_summarize");
+const submitButton = document.getElementById("submit-button");
+const summarizedTextArea = document.getElementById("summary");
 
+submitButton.disabled = true;
+
+textArea.addEventListener("input", verifyTextLength);
+submitButton.addEventListener("click", submitData);
+
+function verifyTextLength(e) {
+  const textarea = e.target;
+
+  // Verify TextArea value
+  if (textarea.value.length > 200 && textarea.value.length < 100000) {
+    // Enable button when text area has value
+    submitButton.disabled = false;
+  } else {
+    // Disable button when text area is empty
+    submitButton.disabled = true;
+  }
+}
+
+function submitData(e) {
+  // this is used to add animation to the submit button
+  submitButton.classList.add("submit-button--loading");
+
+  const text_to_summarize = textArea.value;
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    text_to_summarize: text_to_summarize,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  // here we can omit the “baseUrl” and just use a relative path to “/summarize”
+  fetch("/summarize", requestOptions)
+    .then((response) => response.text()) // res is summarized text
+    .then((summary) => {
+      // update output text area
+      summarizedTextArea.value = summary;
+
+      // stop loading animation to button
+      submitButton.classList.remove("submit-button--loading");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
