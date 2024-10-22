ff559686d82430a3f3894e79e4ca340df323ec93
Anurag Adhikari <anuragadhikari5276@gmail.com>
2024-10-22 20:28:12 +0545

    server side done

---
+++ Server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const summarizeText = require("./summarize.js");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+const app = express();
+app.use(express.json());
+app.use(express.static("public")); // Serve static files from the public
+
+app.post("/summarize", (req, res) => {
+  const text = req.body.text_to_summarize;
+  //   console.log(text);
+
+  // call summarize func and pass text from req
+  summarizeText(text)
+    .then((response) => {
+      res.send(response); // send sumary as res
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
--- server/summarize.js
+++ server/summarize.js
@@ -1,1 +1,32 @@
+const axios = require("axios");
 
+async function summarizeText(text) {
+  let data = JSON.stringify({
+    inputs: text,
+    parameters: {
+      max_length: 100,
+      min_length: 30,
+    },
+  });
+
+  // A config object that contains instructions for API call
+  let config = {
+    method: "post",
+    url: "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.ACCESS_TOKEN,
+    },
+    data: data,
+  };
+
+  try {
+    const response = await axios.request(config);
+    // Return the summary text from res
+    return response.data[0].summary_text;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = summarizeText;
